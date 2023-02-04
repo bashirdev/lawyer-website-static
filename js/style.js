8b65bfcb5e1addc5db515500f656741533817963
bashir ahammed <bahammed16@gmail.com>
2023-02-05 00:25:05 +0400

    bottom to top button added

--- js/style.js
+++ js/style.js
@@ -1,8 +1,8 @@
 let link =document.querySelectorAll('.nav-link')
-
+let backToTop=document.querySelector('.backToTop')
 link.forEach((item, index)=>{
     
-    item.addEventListener("click", function(){
+    item.addEventListener("mouseover", function(){
             removeActive()
             item.classList.add('active')
         
@@ -16,3 +16,37 @@ function removeActive(){
         item.classList.remove('active')
     })
 }
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY < 200){
+        document.querySelector('.backToTop').style.dispaly='hidden' 
+        document.querySelector('.backToTop').style.opacity=0
+    }else{
+        document.querySelector('.backToTop').style.dispaly='block' 
+        document.querySelector('.backToTop').style.opacity=1
+    }
+})
+
+let colors=['green', 'blue', 'purple', 'orange'];
+
+function changeColor(color) {
+    setTimeout(function loop() {
+        
+      if (color.length) {
+        document.querySelector('.backToTop').style.backgroundColor = color.shift();
+        setTimeout(loop, 2000);
+      }
+    
+     
+    }, 2000);
+ 
+
+  }
+
+  function start() {
+    setInterval(changeColor(colors), 3000);
+  }
+  start()
+  
+  
+
